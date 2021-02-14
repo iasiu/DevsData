099c7e324c8c22b958b7135a22c9ae7e47afb784
Jan Lewandowski <iasiu@icloud.com>
2021-02-14 16:41:32 +0100

    added book button, and simple app response for route book

--- app.py
+++ app.py
@@ -12,5 +12,12 @@ def index():
     r = requests.get('http://localhost:5000/events')
     return render_template('index.html', events=json.loads(r.text))
 
+@app.route('/book/<id>', methods=['POST', 'GET'])
+def book(id):
+    r = requests.get('http://localhost:5000/events/{}'.format(id))
+    return r.text
+    #return render_template('index.html', events=json.loads(r.text))
+
+
 if __name__ == "__main__":
     app.run(port=4000)
